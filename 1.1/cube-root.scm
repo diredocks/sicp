8c22d8ab78946ff86df61fc1e121958f1b80caec
diredocks <26994007+diredocks@users.noreply.github.com>
2025-01-22 19:53:17 +0800

    init

---
+++ 1.1/cube-root.scm
@@ -0,0 +1,18 @@
+(define (one-third x y) 
+  (/ (+ x y) 3))
+
+(define (improve guess x)
+  (one-third (/ x (square guess)) (* 2 guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (improve guess x) guess)) 0.001))
+
+(define (cube-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cube-iter (improve guess x) x)))
+
+(define (cube x)
+  (cube-iter 1.0 x))
+
+(cube 4096)
---
+++ README.md
@@ -0,0 +1,1 @@
+> In respect and admiration, to the spirit that lives in the computer.

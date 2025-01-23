d15ffe7893c029ffade4f141ccce0884a76732ce
diredocks <26994007+diredocks@users.noreply.github.com>
2025-01-23 23:39:46 +0800

    1.2/add/expt/fast-iter

--- 1.2/expt.rkt
+++ 1.2/expt.rkt
@@ -19,14 +19,26 @@
   (iter x n 1)
 )
 
-(define (expt-fast x n)
+(define (expt-fast-rec x n)
   (cond
     ((= n 0) 1)
-    ((even? n) (square (expt-fast x (/ n 2))))
-    (else (* x (expt-fast x (- n 1))))
+    ((even? n) (square (expt-fast-rec x (/ n 2))))
+    (else (* x (expt-fast-rec x (- n 1))))
   )
 )
 
-(expt-rec 4 2)
-(expt-iter 4 2)
-(expt-fast 4 2)
+(define (expt-fast-iter x n)
+  (define (iter x n p)
+    (cond
+      ((= n 0) p)
+      ((even? n) (iter (square x) (/ n 2) p))
+      (else (iter x (- n 1) (* x p)))
+    )
+  )
+  (iter x n 1)
+)
+
+(expt-rec 2 10)
+(expt-iter 2 10)
+(expt-fast-rec 2 10)
+(expt-fast-iter 2 10)

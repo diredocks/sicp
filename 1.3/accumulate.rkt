13b8f7f9cda42fd5dbe05ace383df1cb3d8b5eb7
diredocks <26994007+diredocks@users.noreply.github.com>
2025-01-26 15:00:57 +0800

    1.3/update/accumulate

--- 1.3/accumulate.rkt
+++ 1.3/accumulate.rkt
@@ -11,7 +11,6 @@
                 term (next a) next b)))
 )
 
-
 (define (sum-rec term a next b)
   (if (> a b)
     0
@@ -62,3 +61,15 @@
 (product-iter identity 1 inc 10)
 (accumulate-iter * 1 identity 1 inc 10)
 
+(define (filtered-accumulate
+          filter? combiner null-value
+          term a next b)
+  (define (iter a result)
+    (if (> a b) result
+        (iter (next a)
+              (combiner (if (filter? a) (term a) null-value) result))
+    ))
+  (iter a null-value)
+)
+
+(filtered-accumulate odd? + 0 identity 1 inc 5)
